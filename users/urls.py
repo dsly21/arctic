e6aa69e450c26579733993186fc46cb10ca783d5
dsly21 <adsl21@mail.ru>
2022-12-20 17:26:24 +0400

    added password resete complete view

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import (
     PasswordChangeView,
     PasswordChangeDoneView,
     PasswordResetConfirmView,
+    PasswordResetCompleteView,
 )
 from django.urls import path, reverse_lazy
 
@@ -53,12 +54,20 @@ urlpatterns = [
     ),
     path(
         'password_reset/<uidb64>/<token>',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html',
+            success_url=reverse_lazy('users:password_reset_complete')
+        ),
         name='password_reset_confirm'
     ),
     path(
         'password_reset/done/',
         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
         name='password_reset_done'
+    ),
+    path(
+        'password_reset/complete/',
+        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        name='password_reset_complete'
     )
 ]
